0c44b9abfc5a8978ff4fc54d6f92c2f46c07b1dd
Genc98 <gencgashi98@gmail.com>
2025-04-13 16:52:33 +0200

    added api

--- Unittest/obj/Debug/net8.0/Unittest.AssemblyInfo.cs
+++ Unittest/obj/Debug/net8.0/Unittest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d70bbdc584706354401c727dddd006c21a20d837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2c1704f4c3d99b63b8c018e2ce501eb586c50d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
