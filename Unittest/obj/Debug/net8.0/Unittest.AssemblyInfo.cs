d6d604fc8cbdc03b0f5d609941f1ece61daaa116
Genc98 <gencgashi98@gmail.com>
2025-04-13 12:28:01 +0200

    latest

--- Unittest/obj/Debug/net8.0/Unittest.AssemblyInfo.cs
+++ Unittest/obj/Debug/net8.0/Unittest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b30071ea6af24b59aff5afc9846264369e88f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc536a6b2bf69e0002bfaef527325f156f39e4bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
