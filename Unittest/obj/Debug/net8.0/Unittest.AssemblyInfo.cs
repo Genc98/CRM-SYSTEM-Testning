48a7af29927d0b21bba1e60fd7eaf5fc6488c714
Genc98 <gencgashi98@gmail.com>
2025-04-15 18:26:23 +0200

    Fungerande tester

--- Unittest/obj/Debug/net8.0/Unittest.AssemblyInfo.cs
+++ Unittest/obj/Debug/net8.0/Unittest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4b1b5a27268c1efa0d337f76159f08034bccce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae3a75e826e4b2938877689b960d7cc7f8bcda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
